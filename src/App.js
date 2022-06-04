41925509e3b575f471c61c9f976683d9518859d3
naman patil <namanpatil1998@gmail.com>
2022-06-05 01:11:28 +0530

    client crud mern

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import Home from './component/Home';
+import Navbar from './component/navbar';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Register from './component/Register';
+import Edit from './component/Edit';
+import Details from './component/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+      <Routes>
+      <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/Register" element={<Register/>}/>
+        <Route exact path="/Edit/:id" element={<Edit/>}/>
+        <Route exact path="/view/:id" element={<Details/>}/>
+      </Routes>
+    </>
   );
 }
 
